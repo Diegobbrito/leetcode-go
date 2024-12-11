86414fa016ac58d48a4d6a7e25f0058b5fb8400e
Diego Brito <dibbrito@gmail.com>
2024-12-11 20:55:43 -0300

    Add package

--- array/SignProductArray.go
+++ array/SignProductArray.go
@@ -1,12 +1,14 @@
+package array
+
 func arraySign(nums []int) int {
-    i := 1
-    for _, num := range (nums){
-        if(num == 0) {
-            return 0
-        } 
-        if(num < 0){
-            i *= -1
-        } 
-    }
-    return i
+	i := 1
+	for _, num := range nums {
+		if num == 0 {
+			return 0
+		}
+		if num < 0 {
+			i *= -1
+		}
+	}
+	return i
 }
