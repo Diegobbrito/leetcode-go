c431b87f51d21739fb50ab32572ca7f88d8dd2ed
Diego Brito <dibbrito@gmail.com>
2024-12-11 20:56:05 -0300

    Create solution for LinkedListCycle

---
+++ linkedlist/LinkedListCycle.go
@@ -0,0 +1,23 @@
+package linkedlist
+
+// https://leetcode.com/problems/linked-list-cycle/
+func hasCycle(head *ListNode) bool {
+
+	slow := head
+	fast := head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+
+	return false
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
